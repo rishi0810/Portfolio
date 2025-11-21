d12ac673cf768f302397485ce93fa4a3eacf2b26
Rishi Raj <rishi.1003raj@gmail.com>
2025-11-21 19:16:47 +0530

    Stupid Syntax Error | Never Editing in Github Again

--- lib/content.ts
+++ lib/content.ts
@@ -75,7 +75,7 @@ export const content = {
         { name: 'Node.js', imageUrl: 'https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white' },
         { name: 'Express', imageUrl: 'https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white' },
         { name: 'Tailwind', imageUrl: 'https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white' },
-        { name: 'MongoDB', imageUrl: 'https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white' }
+        { name: 'MongoDB', imageUrl: 'https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white' },
         { name: 'Git', imageUrl: 'https://img.shields.io/badge/Git-F05032?style=for-the-badge&logo=git&logoColor=white' }
     ]
   },
